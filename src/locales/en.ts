a7c350338c615329e284e2ac1f1117a269460e9c
naiba <hi@nai.ba>
2022-11-04 14:44:20 +0800

    🌐 update

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,6 +1,6 @@
 export default {
     ui: {
-        vision: "Like people who like to collect stamps, watches and cars, a big part of naiba's hobby is collecting domain names.Most of my domain names were purchased with the intention of conceiving a product based on the meaning of the domain name, with the intention of actually making the product later.But I can't use all of them because of my general level of ability.",
+        vision: "Like people who like to collect stamps, watches and cars, a big part of naiba's hobby is collecting domain names.Most of my domain names were purchased with the intention of conceiving a product based on the meaning of the domain name, with the intention of actually making the product later. However, due to my limited energy, I cannot fully use all the domains.",
         get_in_touch: "Get in touch with naiba",
         the_domain_name: "The domain name",
         owned_by: "is owned by naiba",
